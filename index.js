91e2604c73a1e3b140c1227f140956eabbe1ad14
Matt Brennan <mattyb1000@gmail.com>
2016-01-15 17:58:17 +0000

    server as module

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 var express = require('express');
 var morgan  = require('morgan');
 var updCron = require('./cron');
+var corsOpen = require('@quarterto/express-cors-wide-open');
+var options204 = require('@quarterto/options-204');
 
 var app = express();
-app.use(morgan('dev'));
-app.use(function(req, res, next) {
-	res.set('access-control-allow-origin', req.headers.origin);
-	res.set('access-control-allow-credentials', true);
-	res.set('access-control-allow-headers', req.headers['access-control-request-headers']);
-	res.set('access-control-allow-method', '*');
-	next();
-});
-app.use(function(req, res, next) {
-	if(req.method !== 'OPTIONS') return next();
-	res.sendStatus(204);
-})
+app.use(corsOpen);
+app.use(options204);
 app.use(require('express-pouchdb')(require('./db').PouchDB));
 
 updCron.start();
 
-var port = process.env.PORT || 3001;
-app.listen(port, console.log.bind(console, `listening on ${port}`));
+if(module === require.main) {
+	var port = process.env.PORT || 3001;
+	app.use(morgan('dev'));
+	app.listen(port, console.log.bind(console, `listening on ${port}`));
+} else module.exports = app;
